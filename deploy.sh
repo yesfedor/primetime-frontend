244a814b38e0fc59397a2285cded88efcb0351c0
Fedor Garanin <yesfedor.go@gmail.com>
2024-10-06 21:21:29 +0300

    Update 14

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 ENVIRONMENT_NAME=production
 BRANCH_NAME=main
 
-cd /home/projects/primetime-frontend-${ENVIRONMENT_NAME}
+echo
+
+cd "/home/projects/primetime-frontend-${ENVIRONMENT_NAME}"
 
 ls -a
 
@@ -12,7 +14,7 @@ git clean -fd
 git reset --hard
 git pull
 
-cp ./environments/${ENVIRONMENT_NAME}.env .env
+cp "./environments/${ENVIRONMENT_NAME}.env" .env
 
 node -v
 nvm --help
@@ -25,5 +27,3 @@ npm run build
 BRANCH_NAME=${BRANCH_NAME} ENVIRONMENT_NAME=${ENVIRONMENT_NAME} PORT=3400 docker-compose up -d --build --force-recreate
 
 docker builder prune -f
-
-
