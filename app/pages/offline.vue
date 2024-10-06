0599a83b1bb59d78c14f37b917162c3918c6836c
Fedor Garanin <yesfedor.go@gmail.com>
2024-10-06 21:09:57 +0300

    Update 10

--- app/pages/offline.vue
+++ app/pages/offline.vue
@@ -18,7 +18,7 @@ const nuxtVersion = ref(nuxtApp.versions.nuxt)
     <p>Host: {{ baseUrl }}</p>
 
     <p>
-      <nuxt-link :to="{ name: 'index' }">Home Page</nuxt-link>
+      <nuxt-link :to="{ name: 'index' }">Back to homepage</nuxt-link>
     </p>
   </section>
 </template>
