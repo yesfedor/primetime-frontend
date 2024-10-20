3a41d336fa023b4ddd37b412d315d13f6ffa528d
Fedor Garanin <yesfedor.go@gmail.com>
2024-10-21 01:03:05 +0300

    yandex script

--- configs/head.config.ts
+++ configs/head.config.ts
@@ -96,6 +96,28 @@ export default <Partial<NuxtOptions['app']['head']>>{
 }
       `,
     },
+    {
+      key: 'yandex-script',
+      type: 'text/javascript',
+      innerHTML: `
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+ m[i].l=1*new Date();
+ for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+ k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+ (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ ym(94316253, "init", {
+      clickmap:true,
+      trackLinks:true,
+      accurateTrackBounce:true
+ });
+      `,
+    },
   ],
-  noscript: [],
+  noscript: [
+    {
+      key: 'yandex-noscript',
+      innerHTML: `<div><img src="https://mc.yandex.ru/watch/94316253" style="position:absolute; left:-9999px;" alt="" /></div>`,
+    },
+  ],
 }
