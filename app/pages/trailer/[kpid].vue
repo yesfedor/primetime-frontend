6449b5b0a4ef3ba9d00211c492a20f9658f73da3
Fedor Garanin <yesfedor.go@gmail.com>
2024-11-03 18:05:21 +0300

    fix

--- app/pages/trailer/[kpid].vue
+++ app/pages/trailer/[kpid].vue
@@ -1,39 +1,60 @@
 <template>
-  <v-dialog
-    v-model:model-value="popupIsOpen"
-    scroll-strategy="block"
-    fullscreen
-    absolute
-    content-class="app-trailer-page"
-    persistent
+  <v-container
+    fluid
+    class="app-page-trailer fill-height app-trailer-page__wrapper wrapper"
   >
-    <v-container fluid class="fill-height bg-black app-trailer-page__wrapper wrapper px-5">
-      <v-row v-if="!isShowError && trailerData">
-        <v-col offset-md="1" align-self="center" cols="12" md="5" class="wrapper__info">
-          <h1 class="text-h6 text-md-4">
+    <v-row class="fill-height">
+      <v-col
+        cols="12"
+        class="fill-height"
+      >
+        <v-card
+          v-if="trailerData"
+          class="w-100 fill-height"
+        >
+          <template #image>
+            <v-img
+              :src="getPosterImageByKinopoiskid(trailerData?.kinopoiskId || 0)"
+              cover
+              position="center center"
+            />
+          </template>
+
+          <v-card-title class="text-center text-h4">
             <span class="text-capitalize">{{ trailerType }}</span>
             <span>{{ trailerData.nameRu }}</span>
-          </h1>
-          <TextClamp
-            :text="trailerData.description"
-            :max-lines="trailerDescriptionLines"
-          >
-            <template #before>
-              <span class="text-h6">{{ $t('trailer.descripton') }}</span>
-            </template>
-          </TextClamp>
-          <AppTraierInfo :item="trailerData" />
-          <div class="text-h6 text-md-4 mt-2 d-flex align-center justify-space-between">
-            <span class="mr-2">{{ $t('trailer.actions.register') }}</span>
-            <v-btn class="mt-1 mt-md-0" @click="routeToAuthPage">{{ $t('auth.register') }}</v-btn>
-          </div>
-        </v-col>
-        <v-col cols="12" md="6" class="wrapper__image">
-          <v-img width="100%" aspect-ratio="16/9" gradient="0deg, rgba(0,0,0,.85) 0%, rgba(0,0,0,.15) 100%"  :src="trailerData.posterUrl" :alt="trailerData.nameRu" />
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-dialog>
+          </v-card-title>
+
+          <v-card-text>
+            <p class="app-page-trailer__description w-lg-50">
+              <span class="text-body-1">
+                {{ $t('trailer.descripton') }}
+              </span>
+              <br>
+              <span class="text-body-1 app-trailer-page__description-text">
+                {{ trailerData.description }}
+              </span>
+            </p>
+
+            <AppTraierInfo
+              v-if="trailerData"
+              :item="trailerData"
+            />
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              variant="flat"
+              class="mt-1 mt-md-0"
+              @click="routeToAuthPage"
+            >
+              {{ $t('auth.register') }}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
@@ -43,52 +64,11 @@ import AppTraierInfo from '@/components/watch/trailer/Info.vue'
 import { capitalizeFirstLetter } from '@/utils/capitalizeFirstLetter.helper'
 import { useTitle } from '@vueuse/core'
 import { useI18n } from 'vue-i18n'
-import { useDisplay } from 'vuetify'
 
-const TextClamp = defineAsyncComponent(() => import('vue3-text-clamp'))
-const display = useDisplay()
 const router = useRouter()
 const route = useRoute()
 const { t } = useI18n()
 useTitle(t('app.loading'))
-
-const popupIsOpen = ref(true)
-const trailerIsLoading = ref(false)
-const trailerData = ref<WatchApiExpandedItem | null>(null)
-
-const loadTrailerData = async () => {
-  trailerIsLoading.value = true
-  const kpid = route.params?.kpid
-  if (!kpid || typeof kpid === 'object') {
-    trailerIsLoading.value = false
-    return false
-  }
-  const result = await watchApi.getTrailerData(kpid)
-  if (result?.kinopoiskId) {
-    useTitle(t('watch.share.title', {
-      type: capitalizeFirstLetter(t(`watch.type.${result.type}`)),
-      title: result.nameRu || result.nameEn,
-      year: result.startYear && result.endYear ? `${result.startYear} - ${result.endYear}` : result.year,
-    }))
-    trailerData.value = result
-  }
-  trailerIsLoading.value = false
-  return result
-}
-
-const isShowError = computed(() => trailerIsLoading.value || !trailerData.value)
-const trailerDescriptionLines = computed(() => {
-  if (display.mdAndUp.value) {
-    return 12
-  }
-  return 3
-})
-const trailerType = computed(() => {
-  if (trailerData.value) {
-    return t(`watch.type.${trailerData.value.type}`) + ': '
-  }
-  return ''
-})
 
 const resolvedAuthUrl = router.resolve({
   name: RouteNamesEnum.watch,
@@ -97,7 +77,7 @@ const resolvedAuthUrl = router.resolve({
   },
 })
 
-const routeToAuthPage = () => {
+function routeToAuthPage() {
   router.push({
     name: RouteNamesEnum.auth,
     query: {
@@ -107,29 +87,52 @@ const routeToAuthPage = () => {
   })
 }
 
-watch(popupIsOpen, (popupState) => {
-  if (!popupState) {
-    routeToAuthPage()
+const trailerQuery = await useAsyncData('trailer-data', async () => {
+  const kpid = route.params?.kpid
+  if (!kpid || typeof kpid === 'object') {
+    return false
   }
+  const result = await watchApi.getTrailerData(kpid)
+  if (result?.kinopoiskId) {
+    useTitle(t('watch.share.title', {
+      type: capitalizeFirstLetter(t(`watch.type.${result.type}`)),
+      title: result.nameRu || result.nameEn,
+      year: result.startYear && result.endYear ? `${result.startYear} - ${result.endYear}` : result.year,
+    }))
+  }
+  return result
 })
 
-await useAsyncData('trailer-data', async () => {
-  return await loadTrailerData()
+const trailerData = computed(() => {
+  return trailerQuery.data.value as WatchApiExpandedItem | null
+})
+
+const trailerType = computed(() => {
+  if (trailerData.value) {
+    return `${t(`watch.type.${trailerData.value.type}`)}: `
+  }
+  return ''
 })
 </script>
 
 <style lang="scss">
-.app-trailer-page {
-  overflow: hidden !important;
-  & &__wrapper.wrapper {
-    .wrapper__content, .wrapper__image {
-      height: 600px;
+.app-page-trailer {
+  &__description-text {
+    display: -webkit-box !important;
+    max-height: calc(32px * 1);
+    text-overflow: ellipsis;
+    overflow: hidden;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+
+    @include bp-tablet-min {
+      max-height: calc(32px * 12);
+      -webkit-line-clamp: 12;
     }
-    .wrapper__image {
-      img {
-        object-fit: none;
-      }
-    }
+  }
+
+  .v-card__image {
+    filter: brightness(75%) !important;
   }
 }
 </style>
