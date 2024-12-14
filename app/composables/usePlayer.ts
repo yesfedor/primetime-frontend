8312961fcd263c4321dd21cf81b52cf2800ad705
Fedor Garanin <yesfedor.go@gmail.com>
2024-12-14 23:10:23 +0300

    update players

--- app/composables/usePlayer.ts
+++ app/composables/usePlayer.ts
@@ -6,17 +6,13 @@ const API_CONFIG = {
 }
 
 export enum PlayerAlias {
-  mediafilm = 'mediafilm',
-  svetacdn = 'svetacdn',
-  cdnmovies = 'cdnmovies',
+  lumex = 'lumex',
   tobaco = 'tobaco',
-  voidboost = 'voidboost',
-  gencit = 'gencit',
 }
 
 type PlayerAliasStrings = keyof typeof PlayerAlias
 
-const playerAliasDefault = PlayerAlias.mediafilm
+const playerAliasDefault = PlayerAlias.tobaco
 const playerAlias = useCookie<PlayerAliasStrings>('player-alias', {
   default() {
     return playerAliasDefault as PlayerAliasStrings
@@ -32,20 +28,12 @@ export function usePlayer(kinopoiskId: Ref<string> | null) {
       return ''
     }
     switch (playerAlias.value) {
-      case PlayerAlias.mediafilm:
-        return `//player.mediafilm.in/LDSZJq4uCNvY?kp_id=${kinopoiskId.value}&domain=${API_CONFIG.host}`
-      case PlayerAlias.svetacdn:
-        return `//player.svetacdn.in/LDSZJq4uCNvY?kp_id=${kinopoiskId.value}&domain=${API_CONFIG.host}`
-      case PlayerAlias.cdnmovies:
-        return `https://ugly-turkey.cdnmovies-stream.online/kinopoisk/${kinopoiskId.value}/iframe`
+      case PlayerAlias.lumex:
+        return `https://p.lumex.pw//LDSZJq4uCNvY?kp_id=${kinopoiskId.value}&domain=${API_CONFIG.host}`
       case PlayerAlias.tobaco:
         return `https://api.tobaco.ws/embed/kp/${kinopoiskId.value}`
-      case PlayerAlias.voidboost:
-        return `//voidboost.tv/embed/${kinopoiskId.value}`
-      case PlayerAlias.gencit:
-        return `https://gencit.info/aim/${kinopoiskId.value}`
       default:
-        return `//player.svetacdn.in/LDSZJq4uCNvY?kp_id=${kinopoiskId.value}&domain=${API_CONFIG.host}`
+        return `https://p.lumex.pw//LDSZJq4uCNvY?kp_id=${kinopoiskId.value}&domain=${API_CONFIG.host}`
     }
   })
 
